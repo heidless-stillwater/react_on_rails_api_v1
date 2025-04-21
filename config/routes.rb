f3337a389af7248a3fad1d949c0490db19d91396
heidless <lockhart.r@gmail.com>
2025-04-21 11:39:26 +0100

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  resources :posts
+  # API Rputes should be in '/api/v1'
+  namespace :api do 
+    namespace :v1 do 
+      resources :posts
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
